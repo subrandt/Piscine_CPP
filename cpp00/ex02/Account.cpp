2be25b8ebdb05833f5c3dd15f1006cbd8ad62b79
Susanne Brandt <subrandt@3G2.42angouleme.fr>
2023-01-26 18:51:30 +0100

    cpp00 ex02 le debut

---
+++ cpp00/ex02/Account.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: subrandt <subrandt@student.42angouleme.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/26 17:50:05 by subrandt          #+#    #+#             */
+/*   Updated: 2023/01/26 18:37:26 by subrandt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+
+static void	_displayTimestamp( void )
+{
+
+}
+
+static int	getNbAccounts( void )
+{
+
+}
+
+static int	getTotalAmount( void )
+static int	getNbDeposits( void )
+static int	getNbWithdrawals( void )
+static void	displayAccountsInfos( void )
+void	makeDeposit( int deposit );
+bool	makeWithdrawal( int withdrawal );
+int		checkAmount( void ) const;
+void	displayStatus( void ) const;
