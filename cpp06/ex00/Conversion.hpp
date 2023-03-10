aca652d2bd93e8e4e0253b4895b9be19994b3014
Susanne Brandt <subrandt@3G4.42angouleme.fr>
2023-03-10 18:05:51 +0100

    cpp06ex01debut

---
+++ cpp06/ex00/Conversion.hpp
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: subrandt <subrandt@student.42angouleme.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/02 17:18:05 by subrandt          #+#    #+#             */
+/*   Updated: 2023/03/03 16:38:04 by subrandt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+# include <iostream>
+
+class Bureaucrat
+{
+	public:
+
+		Bureaucrat(void);
+		Bureaucrat(std::string name, int grade);
+		Bureaucrat(Bureaucrat const & src);
+		~Bureaucrat(void);
+
+		Bureaucrat & operator=(Bureaucrat const & rhs);
+
+		const std::string	getName(void) const;
+		int 				getGrade(void) const;
+		int					upGrade(void);
+		int					downGrade(void);
+
+	private:
+		const std::string _name;
+		int	_grade;
+
+		
+		class GradeTooHighException : public std::exception
+		{
+			public:
+				virtual const char* what() const throw();
+		};
+
+		class GradeTooLowException : public std::exception
+		{
+			public:
+				virtual const char* what() const throw();
+		};
+
+
+};
+
+std::ostream & operator<<(std::ostream & o, Bureaucrat const & rhs);
+
+#endif
