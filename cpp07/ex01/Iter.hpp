0850cbb56094778aa868400ee9fb4a518675d2f3
Susanne Brandt <subrandt@3G4.42angouleme.fr>
2023-03-27 14:02:23 +0200

    cpp07exi

--- cpp07/ex01/Iter.hpp
+++ cpp07/ex01/Iter.hpp
@@ -6,7 +6,7 @@
 /*   By: subrandt <subrandt@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/24 15:45:40 by subrandt          #+#    #+#             */
-/*   Updated: 2023/03/24 18:27:43 by subrandt         ###   ########.fr       */
+/*   Updated: 2023/03/27 13:51:07 by subrandt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,21 @@
 # define ITER_HPP
 
 #include <iostream>
+#include <cctype>
 
 template<typename ITER>
 void	ft_mult(ITER & value)
 {
 	std::cout << value * 2 << std::endl;
+}
+
+template<typename ITER>
+void	ft_toupper(ITER & value)
+{
+	std::string str = value;
+	for(int i = 0; i < (int)str.length(); i++)
+		std::cout << (char)toupper(str[i]);
+	std::cout << std::endl;
 }
 
 template<typename ITER>
@@ -28,7 +38,7 @@ void	ft_print(ITER & value)
 }
 
 template<typename ITER>
-void	iter(ITER *array, ITER len, void (*fun)(ITER &))
+void	iter(ITER *array, int len, void (*fun)(ITER &))
 {
 	for (int i = 0; i < len; i++)
 	{
--- cpp07/ex01/main.cpp
+++ cpp07/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: subrandt <subrandt@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/24 15:15:07 by subrandt          #+#    #+#             */
-/*   Updated: 2023/03/24 18:40:42 by subrandt         ###   ########.fr       */
+/*   Updated: 2023/03/27 13:54:32 by subrandt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,28 +15,28 @@
 int	main(void) 
 {
 
-	std::cout << "\nint array: ";
+//Use templates to do something on the array values:
+
+//1) Print array of integer and multiply values:
+	std::cout << "\n -> int array: ";
 	int array[5] = {1, 2, 3, 4, 5};
 	int len = sizeof(array)/sizeof(array[0]);
 	
-	// std::cout << "array : { ";
-	// for(int i = 0; i < len ; i++)
-	// {
-	// 	std::cout << array[i] << " " ;
-	// }
-	// std::cout << "}" << std::endl;
-	// std::cout << "length of array = " << len << std::endl;
-
 	std::cout << "\nprint initial array" << std::endl;
 	iter(array, len, ft_print);
 	std::cout << "\nprint value * 2" << std::endl;
 	iter(array, len, ft_mult);
 
-	// std::cout << "\nstring array: ";
-	// std::string str_array[3] = {"hello", "coucou", "ciao"};
 
-	// std::cout << "\nprint initial array" << std::endl;
-	// iter(str_array, len, ft_print);
+//2) Print array of std::string and put them to uppercase:
+	std::cout << "\n -> string array: ";
+	int len_str = 3;
+	std::string str_array[len_str] = {"hello", "coucou", "ciao"};
+
+	std::cout << "\nprint initial array" << std::endl;
+	iter(str_array, len_str, ft_print);
+	std::cout << "\nprint array in uppercase" << std::endl;
+	iter(str_array, len_str, ft_toupper);
 
 	return (0);
 }
